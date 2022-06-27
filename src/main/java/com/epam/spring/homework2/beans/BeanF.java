d4338a230108b86df6ed834a613b407251d70258
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-06-27 23:04:07 +0300

    refactoring

---
+++ src/main/java/com/epam/spring/homework2/beans/BeanF.java
@@ -0,0 +1,37 @@
+package com.epam.spring.homework2.beans;
+
+
+public class BeanF {
+
+    private String name;
+    private int value;
+
+    public BeanF(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "BeanF{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
