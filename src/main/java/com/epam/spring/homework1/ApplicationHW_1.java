11689d502de8eb87592ee301c3501dcebe5fb3c8
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-07-05 22:20:19 +0300

    code refactoring

--- src/main/java/com/epam/spring/homework1/ApplicationHW_1.java
+++ src/main/java/com/epam/spring/homework1/ApplicationHW_1.java
@@ -3,12 +3,12 @@ package com.epam.spring.homework1;
 import com.epam.spring.homework1.config.BeansConfig;
 import com.epam.spring.homework1.pet.Cheetah;
 import com.epam.spring.homework1.pet.Pet;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationHW_1 {
     public static void main(String[] args) {
+
         ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
         context.getBean(Pet.class).printPets();
 
