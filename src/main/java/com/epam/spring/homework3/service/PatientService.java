786cd5df191dbc3ca77c87bf15e6968125d6f9fc
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-07-14 09:20:31 +0300

    add PatientService

---
+++ src/main/java/com/epam/spring/homework3/service/PatientService.java
@@ -0,0 +1,10 @@
+package com.epam.spring.homework3.service;
+
+import com.epam.spring.homework3.service.model.Patient;
+
+import java.util.List;
+
+public interface PatientService {
+
+    List<Patient> listPatients();
+}
---
+++ src/main/java/com/epam/spring/homework3/service/impl/PatientServiceImpl.java
@@ -0,0 +1,24 @@
+package com.epam.spring.homework3.service.impl;
+
+import com.epam.spring.homework3.service.PatientService;
+import com.epam.spring.homework3.service.model.Patient;
+import com.epam.spring.homework3.service.repository.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PatientServiceImpl implements PatientService {
+
+    private final PatientRepository patientRepository;
+
+    @Override
+    public List<Patient> listPatients() {
+        log.info("PatientServiceImpl.listPatients()");
+        return patientRepository.findAll();
+    }
+}
