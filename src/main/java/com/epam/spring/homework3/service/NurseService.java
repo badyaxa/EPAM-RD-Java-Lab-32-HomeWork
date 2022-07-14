cd167e22f2f42da4d5ecf079b765776b1c0e2afd
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-07-14 09:03:47 +0300

    add NurseService

---
+++ src/main/java/com/epam/spring/homework3/service/NurseService.java
@@ -0,0 +1,10 @@
+package com.epam.spring.homework3.service;
+
+import com.epam.spring.homework3.service.model.Nurse;
+
+import java.util.List;
+
+public interface NurseService {
+
+    List<Nurse> listNurses();
+}
---
+++ src/main/java/com/epam/spring/homework3/service/impl/NurseServiceImpl.java
@@ -0,0 +1,27 @@
+package com.epam.spring.homework3.service.impl;
+
+import com.epam.spring.homework3.service.NurseService;
+import com.epam.spring.homework3.service.model.Nurse;
+import com.epam.spring.homework3.service.repository.NurseRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NurseServiceImpl implements NurseService {
+
+    //final
+    private final NurseRepository nurseRepository;
+
+    public List<Nurse> listNurses() {
+        log.info("NurseServiceImpl.listNurses");
+        return nurseRepository.findAll()
+                .stream()
+                .collect(Collectors.toList());
+    }
+}
